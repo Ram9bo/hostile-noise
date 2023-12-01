f6bfbbee7ebf643f83f1fd4934aaec1a3ff2704c
izergis <38942342+izergis@users.noreply.github.com>
2023-12-01 10:34:26 +0100

    plot argmax
    can be later changed to hostile-likelyhood

--- ClassifyLive.py
+++ ClassifyLive.py
@@ -1,6 +1,8 @@
 import io
 import os
-
+import time
+import keyboard
+import sys
 
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Dense, Flatten, Dropout
@@ -12,8 +14,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import keras
-
-from tqdm import tqdm
 
 import sounddevice as sd
 from scipy.io.wavfile import write
@@ -27,22 +27,77 @@ def process_audio(y, sr):
 
     return reshaped_spectrogram
 
+def get_sound():
 
-if __name__ == "__main__":
-
-    model = keras.models.load_model('Model/model.keras')
+    ext = 1 # exit flag
     sr = 22050  # Sample rate
     seconds = 5  # Duration of recording
 
+    #record 5 seconds of sound
+    y = sd.rec(int(seconds * sr), samplerate=sr, channels=1)
+
+    #exit loop
+    t_end = time.time() + 4.9
+    while time.time() < t_end:
+        if keyboard.is_pressed('q'):
+            return(0, -1)
+    #wait till recording is finished
+    sd.wait()
+
+    #save and load latest wavfile
+    write('output.wav', sr, y)
+    y, sr = librosa.load('output.wav')
+
+    #process audio to correct shape
+    spectrogram = process_audio(y, sr)
+    spectrograms = np.array(spectrogram)
+    spectrograms = spectrograms[None,:,:,:]
+    sound = tf.convert_to_tensor(spectrograms, dtype=tf.float32)
+
+    return sound, ext
+
+
+def plot_loop():
+
+    #load model
+    model = keras.models.load_model('Model/model.keras')
+
+    #set up plot
+    plt.axis([0, 20, 0, 50]) 
+    plt.title("Live classification", fontsize=20)
+    plt.xlabel("time")
+    plt.ylabel("hostile-likelyhood")
+    yar = []
+    xar = []
+    x = 1
+
     while(True):
     
-        y = sd.rec(int(seconds * sr), samplerate=sr, channels=1)
-        sd.wait()
-        write('output.wav', sr, y)
-        y, sr = librosa.load('output.wav')
-        spectrogram = process_audio(y, sr)
-        spectrograms = np.array(spectrogram)
-        spectrograms = spectrograms[None,:,:,:]
-        test = tf.convert_to_tensor(spectrograms, dtype=tf.float32)
-        score = model.predict(test, verbose=0)
-        print(np.argmax(score))
+        sound, ext = get_sound()
+
+        if ext == -1:
+            plt.close()
+            return
+
+        score = model.predict(sound, verbose=0)
+
+        #get last 20 scores
+        if x <= 20 :
+            xar.append(x)
+        else:
+            yar.pop(0)
+        yar.append(np.argmax(score))
+        x +=1
+
+        #plot data
+        plt.clf()
+        plt.plot(xar, yar)
+        plt.pause(0.01)
+        
+
+
+if __name__ == "__main__":
+
+    plot_loop()
+
+  
