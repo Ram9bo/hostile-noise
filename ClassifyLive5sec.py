0eeb6bd66cb0d7a48e52cab3d820b6ab1d2a9cdd
izergis <38942342+izergis@users.noreply.github.com>
2023-12-03 16:57:24 +0100

    Good4Demo

--- ClassifyLive5sec.py
+++ ClassifyLive5sec.py
@@ -12,12 +12,14 @@ import sys
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Dense, Flatten, Dropout
 from keras.models import Sequential
+from scipy.interpolate import make_interp_spline
 
 import librosa
 import librosa.display
 import wave
 import numpy as np
 import matplotlib.pyplot as plt
+
 import tensorflow as tf
 import keras
 
@@ -55,8 +57,6 @@ def get_sound():
     write('output.wav', sr, y1)
     y, sr1 = librosa.load('output.wav')
     
-    #y, sr1 = librosa.load('1-137-A-32.wav') non hostile
-    #y, sr1 = librosa.load('1-9886-A-49.wav')  hostile
 
     #process audio to correct shape
     spectrogram = process_audio(y, sr)
@@ -72,26 +72,22 @@ def plot_loop():
     #load model
     model = keras.models.load_model('Model/model.keras')
 
-    #set up plot
 
-    plt.title("Live classification", fontsize=20)
-    plt.xlabel("time")
-    plt.ylabel("hostile-likelyhood")
     yar = []
     xar = []
     x = 0
 
     while(True):
     
+        #get sound
         sound, ext = get_sound()
 
+        #quit has been called
         if ext == -1:
             plt.close()
             return
 
-        if ext == 1:
-            continue
-
+        #predict
         score = model.predict(sound, verbose=0)
 
         #get last 20 scores
@@ -99,17 +95,40 @@ def plot_loop():
             xar.append(x)
         else:
             yar.pop(0)
-        yar.append(score[0])
+        yar.append(score[0][0])
         print(score)
         x +=1
 
         #plot data
         plt.clf()
-        plt.axis([0, 20, 0, 1]) 
-
+        plt.axis([0, 20, 0, 1])
+        plt.title("Live classification", fontsize=20)
+        plt.xlabel("5-second intervals")
+        plt.ylabel("hostile-likelyhood")
         plt.autoscale(False)
-        plt.plot(xar, yar)
-        plt.pause(0.01)
+
+        #average line
+        plt.axhline(y = np.mean(yar), color = 'r', linestyle = 'dashed')
+
+     
+        #trendline
+        if len(xar) >=2:
+            z = np.polyfit(xar, yar, 1)
+            p = np.poly1d(z)
+            plt.plot(xar, p(xar), color="purple", linewidth=3, linestyle=":")
+
+        #smooth line
+        if len(xar) >=4:
+            X_Y_Spline = make_interp_spline(xar, yar)
+            X_ = np.linspace(min(xar), max(xar), 500)
+            Y_ = X_Y_Spline(X_)
+            plt.plot(X_, Y_)
+            plt.pause(0.01)
+
+        #starting up
+        else:
+            plt.plot(xar, yar)
+            plt.pause(0.01)
         
 
 
